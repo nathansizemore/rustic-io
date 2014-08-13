5906ad5060efe4dcae48c5da83a94177ecf0344d
Administrator <realart@Administrators-MacBook-Pro.local>
2014-08-13 08:42:54 -0400

    License change.

--- src/lib/rustic_io.rs
+++ src/lib/rustic_io.rs
@@ -1,24 +1,16 @@
-// The MIT License (MIT)
+// DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+// Version 2, December 2004
 
-// Copyright (c) 2014 Nathan Sizemore
+// Copyright (C) 2014 Nathan Sizemore <nathanrsizemore@gmail.com>
 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+// Everyone is permitted to copy and distribute verbatim or modified
+// copies of this license document, and changing it is allowed as long
+// as the name is changed.
 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+// TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+// 0. You just DO WHAT THE FUCK YOU WANT TO.
 
 
 extern crate rust_crypto = "rust-crypto";
@@ -31,9 +23,11 @@ use self::rust_crypto::digest::Digest;
 use self::rust_crypto::sha1::Sha1;
 use self::serialize::base64::{ToBase64, STANDARD};
 use self::server::Server;
-//use self::action::Action;
+use self::action::Action;
+use self::server::socket::Socket;
+use self::server::event::Event;
 
-//mod action;
+mod action;
 pub mod server;
 
 
@@ -70,7 +64,7 @@ pub fn start(server: Server, ip: &str, port: u16) {
             }
             Ok(stream) => {
                 spawn(proc() {                
-                    process_new_connection(stream, to_conn_pool.clone())
+                    //process_new_connection(stream, to_conn_pool.clone())
                 })
             }
         }
@@ -86,21 +80,19 @@ pub fn start(server: Server, ip: &str, port: u16) {
  *     - On connect/disconnect - issues out new socket array to event loop
  */
 fn connection_pool(from_server: Receiver<Action>, to_event_loop: Sender<Vec<Socket>>) {
-    let mut connection_list: Vec<Socket>> = Vec::new();
+    let mut connection_list: Vec<Socket> = Vec::new();
     loop {
         // Do all the things forever
     }
 }
-
-
 
 /*
  * Event Loop
  *     - Listens for new socket array generated from Connection Pool
  *     - Cycles through socket array's stream checking for data
  */
-fn event_loop(server: Server, from_conn_pool: Receiver<Vec<Socket>>) {
-
+fn event_loop(events: Vec<Event>, from_conn_pool: Receiver<Vec<Socket>>) {
+    // Wizard magic
 }
 
 
